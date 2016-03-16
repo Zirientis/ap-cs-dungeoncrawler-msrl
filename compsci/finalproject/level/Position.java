2fd665178e42294a0c5cc0060ee0c4a47339ddef
Zirientis <Zirientis@users.noreply.github.com>
2016-03-15 20:13:06 -0700

    Add position class.

---
+++ compsci/finalproject/level/Position.java
@@ -0,0 +1,30 @@
+package compsci.finalproject.level;
+
+
+/**
+ * Write a description of class Position here.
+ * 
+ * @author Michael Shuen, Ryan Later
+ * @version 3/15/2016
+ */
+public class Position
+{
+    private int row;
+    private int col;
+    
+    public Position(int theRow, int theColumn)
+    {
+        row = theRow;
+        col = theColumn;
+    }
+    
+    public int getRow()
+    {
+        return row;
+    }
+    
+    public int getColumn()
+    {
+        return col;
+    }
+}
