0662f37fb0b83885ab8cf682c716208813bccdfc
Michael Shuen <Michael.Shuen@students.rsd.edu>
2016-03-17 10:11:00 -0700

    Add constructors for Level

--- compsci/finalproject/level/Level.java
+++ compsci/finalproject/level/Level.java
@@ -9,9 +9,16 @@ package compsci.finalproject.level;
  */
 public class Level
 {
+    private short[][] data;
     
     public Level(Room single)
     {
-        
+        data = single.getData();
+    }
+
+    // This expects levelMap to not be changed after calling.
+    public Level(short[][] levelMap)
+    {
+        data = levelMap;
     }
 }
